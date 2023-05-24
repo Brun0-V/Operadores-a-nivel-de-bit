b8a85cc795625c117b16ab89924283a5359368b1
Bruno <brunovega006@gmail.com>
2023-05-23 21:36:05 -0300

    Finished EJ11.c

--- Ejercicios/EJ-11.c
+++ Ejercicios/EJ-11.c
@@ -1,44 +1,27 @@
 #include <stdio.h>
 
-void printBinary(int num) {
-    if (num == 0) {
-        printf("0");
-        return;
-    }
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  ((byte) & 0x80 ? '1' : '0'), \
+  ((byte) & 0x40 ? '1' : '0'), \
+  ((byte) & 0x20 ? '1' : '0'), \
+  ((byte) & 0x10 ? '1' : '0'), \
+  ((byte) & 0x08 ? '1' : '0'), \
+  ((byte) & 0x04 ? '1' : '0'), \
+  ((byte) & 0x02 ? '1' : '0'), \
+  ((byte) & 0x01 ? '1' : '0') 
 
-    int bits[8];
-    int i = 0;
+int logicalNAND(int num1, int num2);
 
-    while (num > 0) {
-        bits[i] = num % 2;
-        num /= 2;
-        i++;
-    }
-
-    for (int j = i - 1; j >= 0; j--) {
-        printf("%d", bits[j]);
-    }
+int main() {
+    int num1, num2;
+    printf("Ingrese el primer número entero (0-255): "); scanf("%d", &num1);
+    printf("Ingrese el segundo número entero (0-255): "); scanf("%d", &num2);
+    printf("Resultado en binario: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(logicalNAND(num1, num2))); printf("\n");
+    return 0;
 }
 
 int logicalNAND(int num1, int num2) {
     int result = ~(num1 & num2);
     return result;
-}
-
-int main() {
-    int num1, num2;
-    
-    printf("Ingrese el primer número entero (0-255): ");
-    scanf("%d", &num1);
-    
-    printf("Ingrese el segundo número entero (0-255): ");
-    scanf("%d", &num2);
-    
-    int result = logicalNAND(num1, num2);
-    
-    printf("Resultado en binario: ");
-    printBinary(result);
-    printf("\n");
-    
-    return 0;
 }
