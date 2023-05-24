7693bbaf5afb35c4a057c9247511d217c8fdfbc9
Bruno <brunovega006@gmail.com>
2023-05-23 21:49:46 -0300

    Minor tweaks

--- Ejercicios/EJ-06.c
+++ Ejercicios/EJ-06.c
@@ -4,17 +4,10 @@ void clearBit(int* num, int position);
 
 int main() {
     int num, position;
-    
-    printf("Ingrese un número entero (0-255): ");
-    scanf("%d", &num);
-    
-    printf("Ingrese la posición del bit (0-7): ");
-    scanf("%d", &position);
-    
+    printf("Ingrese un número entero (0-255): "); scanf("%d", &num);
+    printf("Ingrese la posición del bit (0-7): "); scanf("%d", &position);
     clearBit(&num, position);
-    
     printf("Resultado: %d\n", num);
-    
     return 0;
 }
 
